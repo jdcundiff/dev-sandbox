76fd5e0fedd4b8de7448fa24740cf9806a370517
Jeremiah Cundiff <jcundiff@bondware.com>
2018-10-13 16:18:01 -0500

    Created react/django (empty) frontend

--- helium-trader/settings.py
+++ helium-trader/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'portfolio',
     'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
--- helium-trader/urls.py
+++ helium-trader/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import include, url
-from rest_framework import routers
 from portfolio import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+import frontend.urls
 
 # Use rest API and routers to handle URL's.
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'', include(frontend.urls)),
 ]
