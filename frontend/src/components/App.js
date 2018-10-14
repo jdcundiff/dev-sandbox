8e3a4b29484f9f4bb2e59dbd20b4336479dcc5a6
Jeremiah Cundiff <jcundiff@bondware.com>
2018-10-14 17:36:37 -0500

    Created simple InfoBox react component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import DataProvider from "./DataProvider";
-import Table from "./Table";
+import InfoBox from "./InfoBox";
 
-const App = () => (
-  <DataProvider endpoint="api/lead/" render={data => <Table data={data}/> } />
-);
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <InfoBox/>
+      </React.Fragment>
+    );
+  }
+}
 
-const wrapper = document.getElementById("app");
-
-wrapper ? ReactDOM.render(<App />, wrapper) : null;
+export default App;
---
+++ frontend/src/components/InfoBox.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class InfoBox extends React.Component {
+  render() {
+    return (
+      <div className="box">
+        <h1 className="title">Jeremiah Cundiff</h1>
+        <h2 className="subtitle">Quantitative Analyst</h2>
+      </div>
+    );
+  }
+}
+
+export default InfoBox;
