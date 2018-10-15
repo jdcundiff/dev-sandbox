5177e5696b12b20a56a24560d6d2b674a628550e
Jeremiah Cundiff <jcundiff@bondware.com>
2018-10-15 16:34:23 -0500

    Completed bio component

--- frontend/src/components/Bio.js
+++ frontend/src/components/Bio.js
@@ -4,9 +4,11 @@ import PropTypes from "prop-types";
 class Bio extends React.Component {
   constructor(props) {
     super(props);
+    this.props = props;
   }
 
   render() {
+    let data = this.props.data[0];
     return (
       <div className="card">
         <div className="card-image">
@@ -16,8 +18,26 @@ class Bio extends React.Component {
         </div>
         <div className="card-content">
           <div className="media-content">
+            <p className="title is-4">{data.first_name + ' ' + data.last_name}</p>
+            <p className="subtitle is-6">{data.title}</p>
           </div>
         </div>
+        <footer className="card-footer">
+          <div className="card-footer-item">
+            <div className="buttons has-addons is-right">
+                <a className="button is-white" href={data.github}>
+                  <span className="icon is-medium">
+                    <i className="fab fa-github"></i>
+                  </span>
+                </a>
+                <a className="button is-white" href={data.linkedin}>
+                  <span className="icon is-medium">
+                      <i className="fab fa-linkedin"></i>
+                  </span>
+                </a>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
