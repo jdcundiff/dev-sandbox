12d94f276f218ada523871fc2fdaf591b300add4
Jeremiah Cundiff <jcundiff@bondware.com>
2018-10-22 22:50:09 -0500

    Cleaned up project card

--- frontend/src/components/ProjectRow.js
+++ frontend/src/components/ProjectRow.js
@@ -7,46 +7,48 @@ class ProjectRow extends React.Component {
       <div className="card">
         <div className="card-content">
           <div className="media">
-            <div className="media-left">
-              <h2 className="title">{project.title}</h2>
+            <div className="media-content">
+              <p className="title is-4">{project.title}</p>
+              <p className="subtitle is-6">{project.description}</p>
             </div>
           </div>
           <div className="content">
-            {project.description}
+          {/*TODO put project updates here*/}
           </div>
-          <div className="media-content">
-            <div className="field is-grouped is-grouped-multiline">
-              <div className="control">
-                <div className="tags has-addons">
-                  <span className="tag is-dark">Project</span>
-                  <a className="tag is-link" href={project.github}>
-                    <i className="fab fa-github"></i>
-                  </a>
+          <div className="media">
+            <div className="media-content">
+              <div className="field is-grouped is-grouped-multiline">
+                <div className="control">
+                  <div className="tags has-addons">
+                    <span className="tag is-dark">Project</span>
+                    <a className="tag is-link" href={project.github}>
+                      <i className="fab fa-github"></i>
+                    </a>
+                  </div>
+                </div>
+
+                {/*TODO: Add db table for stack and remove hardcoding */}
+                <div className="control">
+                  <div className="tags has-addons">
+                    <span className="tag is-dark">React</span>
+                    <span className="tag is-info">16.5.2</span>
+                  </div>
+                </div>
+
+                <div className="control">
+                  <div className="tags has-addons">
+                    <span className="tag is-dark">Django</span>
+                    <span className="tag is-info">2.1.2</span>
+                  </div>
+                </div>
+
+                <div className="control">
+                  <div className="tags has-addons">
+                    <span className="tag is-dark">Python</span>
+                    <span className="tag is-info">3.6.6</span>
+                  </div>
                 </div>
               </div>
-
-              {/*TODO: Add db table for stack and remove hardcoding */}
-              <div className="control">
-                <div className="tags has-addons">
-                  <span className="tag is-dark">React</span>
-                  <span className="tag is-info">16.5.2</span>
-                </div>
-              </div>
-
-              <div className="control">
-                <div className="tags has-addons">
-                  <span className="tag is-dark">Django</span>
-                  <span className="tag is-info">2.1.2</span>
-                </div>
-              </div>
-
-              <div className="control">
-                <div className="tags has-addons">
-                  <span className="tag is-dark">Python</span>
-                  <span className="tag is-info">3.6.6</span>
-                </div>
-              </div>
-
             </div>
           </div>
         </div>
