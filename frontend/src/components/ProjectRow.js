a317ebfa6d368c94c228785c3c163b9175fc23d6
Jeremiah Cundiff <jcundiff@bondware.com>
2018-10-22 22:29:59 -0500

    Updated project tags for better viewing

--- frontend/src/components/ProjectRow.js
+++ frontend/src/components/ProjectRow.js
@@ -10,17 +10,44 @@ class ProjectRow extends React.Component {
             <div className="media-left">
               <h2 className="title">{project.title}</h2>
             </div>
-            <div className="media-content">
-              <div className="subtitle tags">
-                <span className="tag is-primary">Project</span>
-                <a className="tag is-link" href={project.github}>
-                  <i className="fab fa-github"></i>
-                </a>
-              </div>
-            </div>
           </div>
           <div className="content">
             {project.description}
+          </div>
+          <div className="media-content">
+            <div className="field is-grouped is-grouped-multiline">
+              <div className="control">
+                <div className="tags has-addons">
+                  <span className="tag is-dark">Project</span>
+                  <a className="tag is-link" href={project.github}>
+                    <i className="fab fa-github"></i>
+                  </a>
+                </div>
+              </div>
+
+              {/*TODO: Add db table for stack and remove hardcoding */}
+              <div className="control">
+                <div className="tags has-addons">
+                  <span className="tag is-dark">React</span>
+                  <span className="tag is-info">16.5.2</span>
+                </div>
+              </div>
+
+              <div className="control">
+                <div className="tags has-addons">
+                  <span className="tag is-dark">Django</span>
+                  <span className="tag is-info">2.1.2</span>
+                </div>
+              </div>
+
+              <div className="control">
+                <div className="tags has-addons">
+                  <span className="tag is-dark">Python</span>
+                  <span className="tag is-info">3.6.6</span>
+                </div>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
