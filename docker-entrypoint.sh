83ad2c46b46856e45388d081333e93b59a86ba24
JD <jdcundiff14@gmail.com>
2020-04-27 09:28:49 -0500

    Updated entrypoint for use later

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-python manage.py migrate        # Apply database migrations
-python manage.py collectstatic --clear --noinput # clearstatic files
-python manage.py collectstatic --noinput  # collect static files
+#!/bin/sh
+npm run build
+python manage.py collectstatic --no-input
--- make.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-npm run build && python manage.py collectstatic --no-input && heroku local
